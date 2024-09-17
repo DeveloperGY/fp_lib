59c85e7c5a3166d6a75c6ec49ec0ddad75da7108
DeveloperGY <dev.garrettyenojr@gmail.com>
2024-09-17 15:08:02 -0400

    Changed Logging Functionality

--- src/logging.rs
+++ src/logging.rs
@@ -1,29 +1,9 @@
-use std::fmt::Display;
-
 use crate::time::UTCTime;
-
-pub enum Log {
-    Info,
-    Warning,
-    Error,
-}
-
-impl Display for Log {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let val = match *self {
-            Log::Info => "INFO",
-            Log::Warning => "WARN",
-            Log::Error => "ERR",
-        };
-
-        write!(f, "{}", val)
-    }
-}
 
 pub struct Logger;
 
 impl Logger {
-    pub fn log(dest: &mut impl std::io::Write, log_type: Log, thread_name: &str, msg: &str) {
+    pub fn info(dest: &mut impl std::io::Write, thread_name: &str, msg: &str) {
         let time = std::time::SystemTime::now();
         let hour = time.get_current_hour_24();
         let minute = time.get_current_minute();
@@ -31,9 +11,43 @@ impl Logger {
 
         write!(
             dest,
-            "[{:0>2}:{:0>2}:{:0>2}] [{}/{}]: {}",
-            hour, minute, second, thread_name, log_type, msg
+            "[{:0>2}:{:0>2}:{:0>2}] \x1b[34m[{}/INFO]: {}\x1b[0m\n",
+            hour, minute, second, thread_name, msg
         )
         .unwrap();
+
+        dest.flush().unwrap();
+    }
+
+    pub fn warn(dest: &mut impl std::io::Write, thread_name: &str, msg: &str) {
+        let time = std::time::SystemTime::now();
+        let hour = time.get_current_hour_24();
+        let minute = time.get_current_minute();
+        let second = time.get_current_second();
+
+        write!(
+            dest,
+            "[{:0>2}:{:0>2}:{:0>2}] \x1b[33m[{}/WARN]: {}\x1b[0m\n",
+            hour, minute, second, thread_name, msg
+        )
+        .unwrap();
+
+        dest.flush().unwrap();
+    }
+
+    pub fn err(dest: &mut impl std::io::Write, thread_name: &str, msg: &str) {
+        let time = std::time::SystemTime::now();
+        let hour = time.get_current_hour_24();
+        let minute = time.get_current_minute();
+        let second = time.get_current_second();
+
+        write!(
+            dest,
+            "[{:0>2}:{:0>2}:{:0>2}] \x1b[31m[{}/ERR]: {}\x1b[0m\n",
+            hour, minute, second, thread_name, msg
+        )
+        .unwrap();
+
+        dest.flush().unwrap();
     }
 }
